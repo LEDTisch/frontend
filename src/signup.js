d8cf1d3796d87b596512e875e65ec3339e1dab3b
Coder246 <tim-arnold@outlook.de>
2021-02-01 20:20:39 +0100

    Added Signup functionality

--- src/signup.js
+++ src/signup.js
@@ -1,7 +1,16 @@
-const signupform = document.getElementById("signupform");
-signupform.onsubmit = (e) => {
-    e.preventDefault();
-    
+const signupButton = document.getElementById("submitButton");
+const nameinput = document.getElementById("usernameinput");
+const emailinput = document.getElementById("emailinput");
+const passwordinput = document.getElementById("passwordinput");
 
+signupButton.onclick = function(e) {
+console.log($("#signupfor").serialize())
+
+const url = `${window.API}/signup?name=${nameinput.value}&email=${emailinput.value}&password=${passwordinput.value}`;
+console.log(url);
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", url, false ); // false for synchronous request
+xmlHttp.send( null );
+console.log(xmlHttp.responseText)
 
 }
