e0b41fc4693d673cababb884fc67b69773a92160
Felixpc <46627355+Felixpc@users.noreply.github.com>
2021-02-06 14:21:13 +0100

    update

--- src/dashboard.js
+++ src/dashboard.js
@@ -14,6 +14,10 @@ if(window.readCookie("session")=='') {
     
     }
 
+var primarySelectionIndex = -1;
+
+var appdataInstalledApps;
+
 const subnavprimary = document.getElementById("subnavprimary");
 const subnavsecoundary = document.getElementById("subnavsecoundary")
 const appdata = document.getElementById("appdata");
@@ -34,6 +38,21 @@ $(document).on('click', '.menubar > li', function (e) {
 $(document).on('click', '.verticalMenu > li', function (e) {
     $(this).parent().children(' li ').children('.activeSubNav').removeClass('activeSubNav');
     $(this).children('a').addClass('activeSubNav');
+
+
+    if( $(this).parent().attr('id') == "subnavprimary") {
+        primarySelectionIndex = $(this).parent().children(' li ').children('.activeSubNav').parent().index();
+
+
+    if(state=="AppDaten"){
+        generateSubNavSecoundaryAppData();
+
+        
+    }
+    subnavsecoundary.style.display = "block";
+
+}
+
 e.preventDefault();
 });
 
@@ -48,9 +67,9 @@ home.onclick= (e) => {
 }
 
 appdata.onclick =  (e) => {
-state="AppDAten"
+state="AppDaten"
 subnavprimary.style.display = "block";
-subnavsecoundary.style.display = "block";
+subnavsecoundary.style.display = "none";
 
 subnavprimary.innerHTML="";
 subnavsecoundary.innerHTML="";
@@ -61,6 +80,7 @@ xmlHttp.open( "GET", url, false );
 xmlHttp.send( null );
 
 const data = JSON.parse(xmlHttp.responseText)
+appdataInstalledApps = data;
 
 for(var i=0;i<data.list.length;i++){
 console.log(data.list[i].name);
@@ -73,7 +93,52 @@ li.insertAdjacentElement('beforeend',a);
 subnavprimary.insertAdjacentElement('beforeend',li);
 
 }
-const div=document.createElement("div");
+
+
+
+
+
+
+}
+
+
+
+settings.onclick = (e) => {
+    state="Settings".
+    subnavprimary.style.display = "none";
+    subnavsecoundary.style.display = "none";
+
+
+
+}
+
+function generateSubNavSecoundaryAppData() {
+    const url=window.API+"/app/getAppScores?session="+window.readCookie("session")+"&appuuid="+appdataInstalledApps.list[primarySelectionIndex].UUID;
+    var xmlHttp_getScores=new XMLHttpRequest();
+    xmlHttp_getScores.open( "GET", url, false);
+    xmlHttp_getScores.send(null);
+    subnavsecoundary.innerHTML="";
+    generateCreateNew();
+    const d=JSON.parse(xmlHttp_getScores.responseText);
+    for(var i=0;i<d.write.length;i++){//write auflisten permission
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = d.write[i].name;
+        li.insertAdjacentElement('beforeend',a);
+        subnavsecoundary.insertAdjacentElement('beforeend',li);
+    }
+
+    for(var i=0;i<d.read.length;i++){//write auflisten permission
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = d.read[i].name;
+        li.insertAdjacentElement('beforeend',a);
+        subnavsecoundary.insertAdjacentElement('beforeend',li);
+    }
+}
+
+function generateCreateNew() {
+    const div=document.createElement("div");
 const ti=document.createElement("input");
 const button=document.createElement("button");
 div.id="scorenamediv"
@@ -86,19 +151,22 @@ div.insertAdjacentElement('beforeend',ti);
 div.insertAdjacentElement('beforeend',button);
 subnavsecoundary.insertAdjacentElement('beforeend',div);
 button.onclick = (e) => {
-console.log("add");
+
+    if(!(ti.value.length>3))  return;
+
+var add_url =window.API+"/app/addScore?session="+window.readCookie("session")+"&name="+ti.value+"&appuuid="+appdataInstalledApps.list[primarySelectionIndex].UUID;
+console.log(add_url);
+
+var xmlHttp_add = new XMLHttpRequest();
+xmlHttp_add.open( "GET", add_url, false ); 
+xmlHttp_add.send( null );
+
+ const add_data = JSON.parse(xmlHttp_add.responseText)
+ if(add_data.success = true)
+ti.value = "";
+
+generateSubNavSecoundaryAppData();
 }
-}
-
-
-
-settings.onclick = (e) => {
-    state="Settings"
-    subnavprimary.style.display = "none";
-    subnavsecoundary.style.display = "none";
-
-
-
 }
 
 devices.onclick  = (e) => {
